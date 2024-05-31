a7f54da49dee2a1a033629cb991b6dea8eac56bf
RedSox2 <samir.z.warner@gmail.com>
2024-05-31 08:54:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -95,6 +95,10 @@ for x, y, n in iter_field(Grid.grid):
 pygame.display.update()
 
 while True:
+<<<<<<< HEAD
     pass
 
 print("done")
+=======
+    pass
+>>>>>>> parent of bc01a3c (Merge branch 'main' of https://github.com/RedSox2/minesweeper)
