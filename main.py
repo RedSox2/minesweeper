9dcb5a9009533036b602c058e9c5e27001cc118c
nobody873 <nobodywastaken873@gmail.com>
2024-05-31 08:59:15 -0700

    fixing

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from classes import *
 
 grid_interval = 40
 
-
-
 def draw_grid(window_w, window_h, color):
     block_size = grid_interval  # Set the size of the grid block
     for width in range(0, window_w, block_size):
@@ -16,6 +14,36 @@ def iter_field(fld):
     return [(x_, y_, n) for x_, a_ in enumerate(fld) for y_, n in enumerate(a_)]
 
 pygame.init()
+
+end_time = 0
+
+X = Y = 20
+
+center = (1, 1)
+
+window_width = X*grid_interval
+window_height = Y*grid_interval
+
+screen = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("MINESWEEPER")
+
+font_size = 350*window_height//1440
+font = pygame.font.Font("8bit.ttf", font_size)
+time_font = pygame.font.Font("8bit.ttf", font_size//3)
+
+win_color = (255, 255, 255)
+lose_color = (255, 0, 0)
+
+win = font.render("You Win :D", False, win_color)
+lose = font.render("You Lost :(", False, lose_color)
+
+win_rect = win.get_rect()
+lose_rect = lose.get_rect()
+
+win_rect.center = lose_rect.center = (window_width // 2, window_height // 2)
+
+grey_grid = (128, 128, 128)
+
 
 class Images:
     unopened = pygame.image.load("unopened.jpg")
@@ -42,59 +70,34 @@ class Images:
             Images.squares[i] = pygame.transform.scale(Images.squares[i], (grid_interval, grid_interval))
 
 Images.scale(grid_interval-1)
-end_time = 0
-
-X = Y = 22
-
-center = (1, 1)
-
-window_width = (X-2)*grid_interval
-window_height = (Y-2)*grid_interval
-
-screen = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("MINESWEEPER")
-
-font_size = 350*window_height//1440
-font = pygame.font.Font("8bit.ttf", font_size)
-time_font = pygame.font.Font("8bit.ttf", font_size//3)
-
-win_color = (255, 255, 255)
-lose_color = (255, 0, 0)
-
-win = font.render("You Win :D", False, win_color)
-lose = font.render("You Lost :(", False, lose_color)
-
-win_rect = win.get_rect()
-lose_rect = lose.get_rect()
-
-win_rect.center = lose_rect.center = (window_width // 2, window_height // 2)
-
-grey_grid = (128, 128, 128)
 
 Grid.generateStartingGrid()
-for row in Grid.grid:
-    for col in row: 
-        print(col, end=" ; ")
-    print("")
 
-draw_grid(grid_interval*20, grid_interval*20, grey_grid)
+draw_grid(grid_interval*X, grid_interval*Y, grey_grid)
 
 for row in range(0+center[0],20+center[1]):
     for col in range(0+center[0],20+center[1]):
-        if (Grid.grid[row][col].value == -1):
-            screen.blit(Images.mine, ((col-1)*grid_interval, (row-1)*grid_interval))
-        else:
-            screen.blit(Images.squares[Grid.grid[row][col].value], ((col-1)*grid_interval, (row-1)*grid_interval))
-'''
-for x, y, n in iter_field(Grid.grid):
-    if n.value == -1:
-        screen.blit(Images.mine, (x*grid_interval, y*grid_interval))
-    else:
-        screen.blit(Images.squares[n.value], (x*grid_interval, y*grid_interval))
-    #screen.blit(Images.unopened, (x*grid_interval, y*grid_interval))'''
+        # if (Grid.grid[row][col].value == -1):
+        #     screen.blit(Images.mine, ((col-center[0])*grid_interval, (row-center[0])*grid_interval))
+        # else: 
+        #     screen.blit(Images.squares[Grid.grid[row][col].value], ((col-center[0])*grid_interval, (row-center[0])*grid_interval))
+        screen.blit(Images.unopened, ((col-center[0])*grid_interval, (row-center[0])*grid_interval))
 pygame.display.update()
 
 while True:
-    pass
-
-print("done")
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            selectedCol = event.pos[0]//grid_interval + center[0]
+            selectedRow = event.pos[1]//grid_interval + center[1]
+            
+            if event.button == 1:
+                selectedSquareValue = Grid.grid[selectedRow][selectedCol].value
+                if selectedSquareValue == -1:
+                    screen.blit(Images.mine, ((selectedCol - center[0])*grid_interval, (selectedRow - center[1])*grid_interval))
+                else:
+                    screen.blit(Images.squares[selectedSquareValue], ((selectedCol - center[0])*grid_interval, (selectedRow - center[1])*grid_interval))
+    pygame.display.update()
