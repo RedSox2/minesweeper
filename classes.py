caffa959926546e60cdb82599e012d9e0b39bb52
nobody873 <nobodywastaken873@gmail.com>
2024-06-04 08:50:44 -0700

    removing x,y from classes

--- classes.py
+++ classes.py
@@ -2,22 +2,20 @@ import pygame
 import random
 
 class GridSquare:
-    def __init__(self, current, value, x, y):
+    def __init__(self, current, value):
         self.value = value
         self.current = current
-        self.x = x
-        self.y = y
         
 
     def __str__(self):
-        return f"{self.current}, {self.value}, {self.x}, {self.y}"
+        return f"{self.current}, {self.value}"
         
-    def getNeighbors(self, X, Y):
-        self.neighbors = [(x2, y2) for x2 in range(self.x - 1, self.x + 2)
-            for y2 in range(self.y - 1, self.y + 2)
-            if (-1 < self.x <= X and
-                -1 < self.y <= Y and
-                (self.x != x2 or self.y != y2) and
+    def getNeighbors(self, X, Y, x, y):
+        self.neighbors = [(x2, y2) for x2 in range(x - 1, x + 2)
+            for y2 in range(y - 1, y + 2)
+            if (-1 < x <= X and
+                -1 < y <= Y and
+                (x != x2 or y != y2) and
                 (-1 < x2 < X) and
                 (-1 < y2 < Y))]
         
@@ -34,15 +32,15 @@ class Grid:
             for col in range(Grid.size[0]):
                 if (random.random() < density):
                     # -1 == mine, -2 == unopened
-                    gridRow.append(GridSquare(-2, -1, col, row))
+                    gridRow.append(GridSquare(-2, -1))
                 else:
-                    gridRow.append(GridSquare(-2, -2, col, row))
+                    gridRow.append(GridSquare(-2, -2))
             Grid.grid.append(gridRow)
 
         for row in range(1,21):
             for col in range(1,21):
                 if (Grid.grid[row][col].value != -1):
-                    Grid.grid[row][col].getNeighbors(Grid.size[0], Grid.size[1])
+                    Grid.grid[row][col].getNeighbors(Grid.size[0], Grid.size[1], col, row)
                     numMines = 0
                     for nx, ny in Grid.grid[row][col].neighbors:
                         if Grid.grid[ny][nx].value == -1:
@@ -50,7 +48,28 @@ class Grid:
                     Grid.grid[row][col].value = numMines
 
     def addTopRow():
-        Grid.center[1] -= 1
+        if (Grid.center[1] > 1):
+            Grid.center[1] -= 1
+        else:
+            newRow = []
+        row = 1
+        for col in range(len(Grid.grid[-1])):
+            if (random.random() <= Grid.density):
+                newRow.append(GridSquare(-2, -1))
+            else:
+                newRow.append(GridSquare(-2, -2))
+
+        Grid.grid.insert(newRow, 1)
+
+        for col in range(1, len(Grid.grid[-2])-1):
+            if (Grid.grid[row][col].value != -1):
+                    Grid.grid[row][col].getNeighbors(Grid.size[0], Grid.size[1], col, row)
+                    numMines = 0
+                    for nx, ny in Grid.grid[row][col].neighbors:
+                        if Grid.grid[ny][nx].value == -1:
+                            numMines += 1
+                    Grid.grid[row][col].value = numMines
+
     
     def addBottomRow():
         
@@ -61,15 +80,15 @@ class Grid:
         row = len(Grid.grid)-1
         for col in range(len(Grid.grid[-1])):
             if (random.random() <= Grid.density):
-                newRow.append(GridSquare(-2, -1, col, row+1))
+                newRow.append(GridSquare(-2, -1))
             else:
-                newRow.append(GridSquare(-2, -2, col, row+1))
+                newRow.append(GridSquare(-2, -2))
 
         Grid.grid.append(newRow)
 
         for col in range(1, len(Grid.grid[-2])-1):
             if (Grid.grid[row][col].value != -1):
-                    Grid.grid[row][col].getNeighbors(Grid.size[0], Grid.size[1])
+                    Grid.grid[row][col].getNeighbors(Grid.size[0], Grid.size[1], col, row)
                     numMines = 0
                     for nx, ny in Grid.grid[row][col].neighbors:
                         if Grid.grid[ny][nx].value == -1:
