9f8c44a59cc8401bb5d29dcd2a1a3ec3ce995848
nobody873 <nobodywastaken873@gmail.com>
2024-06-06 09:57:21 -0700

    adding stuff to end loop

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import math
 import pygame
 from classes import *
-from cryptography.fernet import Fernet
+#from cryptography.fernet import Fernet
 from datetime import datetime
 from time import sleep
 
 grid_interval = 40
 
-f = Fernet('nIdceQJdk0GCuhLi31hP8k_K00MtIwveEHszdpM1I24='.encode())
+#f = Fernet('nIdceQJdk0GCuhLi31hP8k_K00MtIwveEHszdpM1I24='.encode())
 
 
 def draw_grid(window_w, window_h, color):
@@ -65,9 +65,9 @@ def lost():
     intScore = round(score)
     print(f"Your score was: {intScore}")
 
-    with open('highscore.txt', 'r') as file:
+    '''with open('highscore.txt', 'r') as file:
         data = file.read().encode()
-    prevHighScore = int(f.decrypt(data).decode().split()[-1])
+    prevHighScore = int(f.decrypt(data).decode().split()[-1])'''
 
                 
     
@@ -87,14 +87,14 @@ def lost():
     screen.blit(score, score_rect)
     pygame.display.update()
     sound.lose.play()
-    if intScore > prevHighScore:
+    '''if intScore > prevHighScore:
         print("You got a highscore!")
         name = input('Please enter your name for the record: ')
         date = str(datetime.now())
 
         toWrite = f'Name: {name}\nDate: {date}\nScore: {intScore}'.encode()
         with open('highscore.txt', 'w') as file:
-            file.write(f.encrypt(toWrite).decode())
+            file.write(f.encrypt(toWrite).decode())'''
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -103,8 +103,13 @@ def lost():
         draw_grid(grid_interval*X, grid_interval*Y, grey_grid)
         for row in range(0+Grid.center[1],20+Grid.center[1]):
             for col in range(0+Grid.center[0],20+Grid.center[0]):
-                screen.blit(Images.squares[Grid.grid[row][col].current], ((col-Grid.center[0])*grid_interval, (row-Grid.center[1])*grid_interval))
+                if Grid.grid[row][col].value == -1:
+                    screen.blit(Images.squares[-1], ((col-Grid.center[0])*grid_interval, (row-Grid.center[1])*grid_interval))
+                else:
+                    screen.blit(Images.squares[Grid.grid[row][col].current], ((col-Grid.center[0])*grid_interval, (row-Grid.center[1])*grid_interval))
         particles.update(1/60)
+        screen.blit(lose, lose_rect)
+        screen.blit(score, score_rect)
         clock.tick(60)
         pygame.display.update()
 
@@ -169,12 +174,12 @@ Grid.generateStartingGrid(0.2)
 draw_grid(grid_interval*X, grid_interval*Y, grey_grid)
 
 
-with open('highscore.txt', 'r') as file:
+'''with open('highscore.txt', 'r') as file:
     highscore = file.read().encode()
     highscore = f.decrypt(highscore).decode()
     print("The current highscore is:")
     print(highscore)
-    sleep(5)
+    sleep(5)'''
 
 while True:
     for event in pygame.event.get():
